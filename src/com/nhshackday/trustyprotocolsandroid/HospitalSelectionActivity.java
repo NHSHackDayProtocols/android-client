a27fda1ecb9c0d34c4ba5b34c283fbaf12324448
Gautier Hayoun <ghayoun@gmail.com>
2013-05-25 14:28:13 +0100

    Add dummy Hospital ListActivity

--- src/com/nhshackday/trustyprotocolsandroid/HospitalSelectionActivity.java
+++ src/com/nhshackday/trustyprotocolsandroid/HospitalSelectionActivity.java
@@ -3,13 +3,21 @@ package com.nhshackday.trustyprotocolsandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.app.ListActivity;
+import android.widget.ArrayAdapter;
 
-public class HospitalSelectionActivity extends Activity {
+public class HospitalSelectionActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hospital_selection);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                R.layout.hospital_row,
+                R.id.text1,
+                new String[]{"Hospital1", "Hostpital2"});
+        setListAdapter(adapter);
 	}
 
 	@Override
